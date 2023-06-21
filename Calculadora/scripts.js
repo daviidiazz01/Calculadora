16fd868e67f3ff5ee9a2836dc70d8dad6ef5c38e
Davi Diaz <felipediaz3974@gmail.com>
2023-06-21 15:11:08 -0300

    Calculadora 1.0

---
+++ Calculadora/scripts.js
@@ -0,0 +1,19 @@
+function inserc(num) {
+    var numero = document.getElementById("Resultado").innerHTML;
+   document.getElementById("Resultado").innerHTML= numero + num;
+  }
+  function limpar(){
+    document.getElementById("Resultado").innerHTML = "";
+  }
+  function voltar() {
+    var resultado = document.getElementById("Resultado").innerHTML;
+    document.getElementById("Resultado").innerHTML = resultado.substring(0, resultado.length - 1);
+  }
+  function calcular(){
+    var resultado = document.getElementById("Resultado").innerHTML;
+    if(resultado){
+        document.getElementById("Resultado").innerHTML = eval(resultado);
+    } else {
+        document.getElementById("Resultado").innerHTML = "Nenhum valor...";
+    }
+}
